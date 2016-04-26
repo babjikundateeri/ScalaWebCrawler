3092e3018d4c849761afa5618ac6905b62c94b14
babjikundateeri <babji.kundateeri@gmail.com>
2016-04-26 14:22:40 +0530

    added filter to the NodeSeq

--- src/main/scala/com/pramati/scala/crawler/utils/WebCrawlerUtils.scala
+++ src/main/scala/com/pramati/scala/crawler/utils/WebCrawlerUtils.scala
@@ -29,56 +29,25 @@ object CrawlerFileUtils {
 object WebCrawlerParser {
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  def checkByAttributeKeyAndValue(e: Node, att: String, value: String): NodeSeq = {
-    def filterAtribute(node: Node, att: String, value: String) =  (node \ ("@" + att)).text == value
-    e \\ "_" filter { n=> filterAtribute(n, att, value)}
-  }
-
-  def parseTR(e: Node): Unit = {
-    println("---------------------")
-    println(e)
-  }
-
   def parseUrlContent(content: String): List[MonthlyDataBean] = {
     logger.debug("in parsing html content")
-    val gridtable: NodeSeq  = scala.xml.XML.loadString(content)   \\ "html" \\ "body" \ "table" \\  "table" \ "tbody" \ "tr"
+    val gridtable: NodeSeq  = (scala.xml.XML.loadString(content)   \\ "html" \\ "body" \ "table" \\  "table" \ "tbody" \ "tr" ).filter(_.text.contains(WebCrawlerProperties.getProperty("Year")))
+
     logger.debug("No of matches found  " + gridtable.length)
 
-
-//    gridtable.head;
-
-
-//    val nodeIterator  = gridtable.iterator
-//    logger.debug("---------- " + nodeIterator.hasNext)
-//    logger.debug("" + nodeIterator)
-//    while (nodeIterator.hasNext) {
-//
-//
-//    }
-    def printNodesInRecusive (nodeSeq: NodeSeq) : List[MonthlyDataBean] = nodeSeq.tail.isEmpty match {
+    def trNodeToMonthlyDataBeanList (nodeSeq: NodeSeq) : List[MonthlyDataBean] = nodeSeq.tail.isEmpty match {
     case false =>
-      val monthlyDataBean = getMonthlyDataBean(nodeSeq.head);
-      monthlyDataBean.id == "0" match {
-        case true => printNodesInRecusive(nodeSeq.tail)
-        case false => printNodesInRecusive(nodeSeq.tail) :+ monthlyDataBean
-      }
-//
-//      printNodesInRecusive(nodeSeq.tail) :+ monthlyDataBean
+      trNodeToMonthlyDataBeanList(nodeSeq.tail) :+ getMonthlyDataBean(nodeSeq.head)
     case true =>
-        List(getMonthlyDataBean(nodeSeq.head))
+      List(getMonthlyDataBean(nodeSeq.head))
     }
-    def getMonthlyDataBean(node: Node) : MonthlyDataBean = {
-      node.text.contains(WebCrawlerProperties.getProperty("Year")) match  {
-        case true =>
-          val id = (node \\ "@id").text
-          val msgcount = (node \\ "td" ).tail.tail.head.text.toInt
-          val href = (node \\ "@href").tail.head.text
-          MonthlyDataBean(id, href, msgcount)
-        case false =>
-          MonthlyDataBean("0", "0", 0)
 
-      }
+    def getMonthlyDataBean(node: Node) : MonthlyDataBean = {
+      val id = (node \\ "@id").text
+      val msgcount = (node \\ "td" ).tail.tail.head.text.toInt
+      val href = (node \\ "@href").tail.head.text
+      MonthlyDataBean(id, href, msgcount)
     }
-    printNodesInRecusive(gridtable)
+    trNodeToMonthlyDataBeanList(gridtable)
   }
 }
