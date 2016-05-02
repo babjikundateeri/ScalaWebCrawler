3d4d2b020e5c3a77211e6bc1e470aadf05c0d319
babjikundateeri <babji.kundateeri@gmail.com>
2016-05-02 12:08:53 +0530

    added small improvement

--- src/main/scala/com/pramati/scala/crawler/service/MonthlyDataBeanWorker.scala
+++ src/main/scala/com/pramati/scala/crawler/service/MonthlyDataBeanWorker.scala
@@ -31,25 +31,25 @@ class MonthlyDataBeanWorker(monthlyDataBean: MonthlyDataTransferObject) extends 
       logger.debug(s"Creating dir .. $outDir ")
       WebCrawlerFileUtils.createDirectories(outDir)
     }
-    // it might contain morethan 1 page
+    // it might contain more than 1 page
     val filesCountInDir = WebCrawlerFileUtils.getNoOfFileInDir(outDir)
     logger.info(s"${bean.href}  -- Mails at local dir  / server dir :: $filesCountInDir /  ${bean.msgCount}")
 
     if (filesCountInDir >= bean.msgCount) {
       List.empty
     } else {
-      val noOfPages: Int = bean.msgCount / WebCrawlerProperties.getNoOfMailsPerPage + 1
+      val noOfPages: Int = (bean.msgCount / WebCrawlerProperties.getNoOfMailsPerPage) + 1
       logger.info(s"${bean.href} No of pages to read  $noOfPages")
 
+      go(noOfPages-1,bean)
+    }
+  }
 
-      def go (pageNumber: Int) : List[MailArchiveDataTransferObject] = {
-        if(pageNumber < 0) List.empty
-        else {
-          val url = s"${WebCrawlerProperties.getURL}${bean.href}?${pageNumber.toString}"
-          WebCrawlerParsingUtils.parseArchivesMailsPage(WebCrawlerUtils.readDataFromURL(url), bean) ::: go (pageNumber -1)
-        }
-      }
-      go(noOfPages-1)
+  private def go (pageNumber: Int,bean: MonthlyDataTransferObject) : List[MailArchiveDataTransferObject] = {
+    if(pageNumber < 0) List.empty
+    else {
+      val url = s"${WebCrawlerProperties.getURL}${bean.href}?${pageNumber.toString}"
+      WebCrawlerParsingUtils.parseArchivesMailsPage(WebCrawlerUtils.readDataFromURL(url), bean) ::: go (pageNumber -1,bean)
     }
   }
 }
